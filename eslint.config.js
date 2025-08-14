dc4024e61212cc9d3b365df201abf24663a21cb0
Ewa Majowska <em.majowska@gmail.com>
2025-08-14 20:48:50 +0400

    Configure gulp for automation process. Correct assets' paths for the correct reading from dist folder

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser, sourceType: 'module' },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
+    },
+  },
+]);
