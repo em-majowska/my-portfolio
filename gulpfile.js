108bac1f1a206a7013e69986379a3c79f684ca2d
Ewa Majowska <em.majowska@gmail.com>
2025-08-22 16:18:16 +0400

    Configure gulp for automation process. Correct assets' paths for the correct reading from dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,144 @@
-const gulp = require('gulp');
-const clean = require('gulp-clean');
-const sass = require('gulp-sass')(require('sass'));
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-// const eslint = require('gulp-eslint-new');
-const deploy = require('gulp-gh-pages');
+import gulp from 'gulp';
+import { deleteAsync } from 'del';
+import htmlmin from 'gulp-htmlmin';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import cssnano from 'cssnano';
+import sourcemaps from 'gulp-sourcemaps';
+import replace from 'gulp-replace';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint-new';
+import uglify from 'gulp-uglify';
+import ghpages from 'gh-pages';
+import { promisify } from 'util';
+import browserSync from 'browser-sync';
+import imagemin, { optipng, svgo } from 'gulp-imagemin';
+import noop from 'gulp-noop';
 
-function cleanDist() {
-  return gulp.src('./dist', { read: false, allowEmpty: true }).pipe(clean());
+const isProd = process.env.NODE_ENV === 'production';
+const sassCompiler = gulpSass(dartSass);
+const publish = promisify(ghpages.publish);
+const sync = browserSync.create();
+const plugins = {
+  dev: [autoprefixer()],
+  prod: [autoprefixer(), cssnano()],
+};
+
+const paths = {
+  html: './src/*.html',
+  styles: './src/scss/**/*.scss',
+  scripts: './src/js/**/*.js',
+  assets: './src/assets/**/*.*',
+};
+
+/* HTML */
+function html() {
+  return gulp
+    .src(paths.html)
+    .pipe(
+      isProd
+        ? htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+          })
+        : gulp.dest('dist')
+    )
+    .pipe(gulp.dest('dist'));
 }
+
+/* Styles */
 function styles() {
   return gulp
-    .src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .src(paths.styles)
+    .pipe(isProd ? noop() : sourcemaps.init())
+    .pipe(sassCompiler().on('error', sassCompiler.logError))
+    .pipe(postcss(isProd ? plugins.prod : plugins.dev))
+    .pipe(replace('../../src/assets', '../assets'))
+    .pipe(isProd ? noop() : sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'));
 }
 
-function html() {
-  return gulp.src('./src/*.html').pipe(gulp.dest('./dist'));
-}
-
+/* Scripts */
 function scripts() {
-  return (
-    gulp
-      .src('./src/js/**/*.js')
-      // .pipe(eslint())
-      // .pipe(eslint.format())
-      // .pipe(eslint.failAfterError())
-      .pipe(
-        babel({
-          presets: ['@babel/preset-env'],
-        })
-      )
-      .pipe(uglify())
-      .pipe(gulp.dest('./dist/js'))
-  );
+  return gulp
+    .src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(babel())
+    .pipe(isProd ? uglify() : gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist/js'));
 }
 
-function assets() {
+/* Assets */
+function otherAssets() {
   return gulp
-    .src('src/assets/**/*', { base: 'src/assets' })
+    .src([paths.assets, `!${paths.assets}/**/*.{png,svg}`], { base: 'src' })
+    .pipe(gulp.dest('dist'));
+}
+
+function images() {
+  return gulp
+    .src('src/assets/**/*.{png,svg}')
+    .pipe(
+      isProd
+        ? imagemin(
+            [
+              svgo({
+                plugins: [
+                  { name: 'removeViewBox', active: false },
+                  { name: 'removeDimensions', active: false },
+                  { name: 'removeUselessDefs', active: false },
+                  { name: 'cleanupIDs', active: false },
+                  { name: 'removeUnknownsAndDefaults', active: false },
+                ],
+              }),
+              optipng(),
+            ],
+            { verbose: true }
+          )
+        : gulp.dest('dist/assets')
+    )
     .pipe(gulp.dest('dist/assets'));
 }
+const assets = gulp.parallel(otherAssets, images);
 
-const build = gulp.series(
-  cleanDist,
-  gulp.parallel(styles, html, assets, scripts)
-);
-exports.build = build;
-
-function deployTask() {
-  return gulp.src('./dist/**/*').pipe(deploy());
+function babelPolyfill() {
+  return gulp
+    .src([
+      './node_modules/core-js-bundle/minified.js',
+      './node_modules/regenerator-runtime/runtime.js',
+    ])
+    .pipe(gulp.dest('dist/polyfills'));
 }
-exports.deploy = gulp.series(build, deployTask);
+
+/* Clean & Serve */
+function cleanDist() {
+  return deleteAsync(['dist']);
+}
+
+function serve() {
+  sync.init({
+    server: 'dist',
+    port: 8080,
+    ui: { port: 8081 },
+  });
+
+  gulp.watch(paths.html, html).on('change', sync.reload);
+  gulp.watch(paths.styles, styles).on('change', sync.reload);
+  gulp.watch(paths.scripts, scripts).on('change', sync.reload);
+}
+
+export const build = gulp.series(
+  cleanDist,
+  gulp.parallel(styles, html, assets, scripts, babelPolyfill)
+);
+
+export const deployGhPages = gulp.series(build, async () => {
+  await publish('dist');
+});
+
+export default gulp.series(build, serve);
